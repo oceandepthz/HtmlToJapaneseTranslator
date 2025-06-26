9c52dab4053df27a76e840bcc70fa1c0d599ba10
Makoto Kozono <oceandepthz@gmail.com>
2025-06-26 16:13:16 +0900

    feat: Implement TranslatorConfig class

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '32036868b5143c982790a70b689bbb2cc2bf26a0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '32036868b5143c982790a70b689bbb2cc2bf26a0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
